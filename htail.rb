ce1f35a18bcd5c2abef86ec08698dd47a9070018
Daniel Black <dyspop@gmail.com>
2017-01-18 11:44:38 -0500

    send the email full of change links

--- htail.rb
+++ htail.rb
@@ -18,13 +18,20 @@ end
 
 loop do
 	site_now = HTTParty.get(site)
+	links = []
 	if site_now.to_s != site_origin.to_s
-		puts "CHANGE FOUND at " + (Time.now).to_s
+		now = (Time.now).to_s
+		puts "CHANGE FOUND at " + now
 		links_origin = get_links(site_origin)
 		links_now = get_links(site_now)
 		for link in links_now
-			puts ("NEW LINK: " + link.to_s) if !(links_origin.include? link)
+			if !(links_origin.include? link)
+				puts ("NEW LINK: " + link.to_s) 
+				links << link
+			end
 		end
+		links = links.to_s
+		(`echo "#{links}" | mail -s "#{site} update at #{now}" #{recipient}`) if links != '[]'
 		site_origin = site_now
 	else
 		puts "no change..."
