712cd23d85774d4022598d20da107c76b70b883c
Daniel Black <dyspop@gmail.com>
2017-01-18 11:24:58 -0500

    fuck diffy, inspect nokogiri urls array

--- htail.rb
+++ htail.rb
@@ -19,11 +19,16 @@ end
 loop do
 	site_now = HTTParty.get(site)
 	if site_now.to_s != site_origin.to_s
-		puts "CHANGE FOUND"
+		puts "CHANGE FOUND at " + (Time.now).to_s
 		links_origin = get_links(site_origin)
 		links_now = get_links(site_now)
-		diff = Diffy::Diff.new(links_origin, links_now)
-		site_origin = site_now
+		for link in links_now
+			if links_origin.include? link
+				puts "old link: " + link.to_s
+			else
+				puts "NEW LINK: " + link.to_s
+			end
+		end
 	else
 		puts "no change..."
 	end
