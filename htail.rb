4290ddbf8f3d3a4c511aa043b81d722ebe44cac9
Daniel Black <dyspop@gmail.com>
2017-01-18 11:07:06 -0500

    use nokogiri for html inspection

--- htail.rb
+++ htail.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'diffy'
+require 'nokogiri'
 
 site = ARGV[0]
 recipient = ARGV[1]
@@ -8,15 +9,23 @@ counter = 1
 
 site_origin = HTTParty.get(site)
 
+def get_links(url)
+  Nokogiri::HTML(url).css("a").map do |link|
+    if (href = link.attr("href")) && href.match(/^https?:/)
+      href
+    end
+  end.compact
+end
+
 loop do
 	site_now = HTTParty.get(site)
 	if Time.now < now + counter
 		next
 	elsif site_now.to_s != site_origin.to_s
-		diff = "<style>" + Diffy::CSS + ".unchanged { display:none !important; } </style>" + Diffy::Diff.new(site_origin, site_now).to_s(:html)
-		puts "mail -s \"changes found in #{site}\" \"#{recipient}\""
+		diff = Diffy::Diff.new(site_origin, site_now).to_s(:html)
+		puts get_links(site)
+		#site_origin = site
 	else
 		puts "no change..."
 	end
-	break
 end
